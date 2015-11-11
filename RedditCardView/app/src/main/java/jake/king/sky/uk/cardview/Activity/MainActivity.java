cece1f22ae6c0add0330730f94ac1e35495670ce
Jake King <jake.king@sky.uk>
2015-11-11 19:56:37 +0000

    Base Project Started

---
+++ RedditCardView/app/src/main/java/jake/king/sky/uk/cardview/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package jake.king.sky.uk.cardview.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jake.king.sky.uk.cardview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
