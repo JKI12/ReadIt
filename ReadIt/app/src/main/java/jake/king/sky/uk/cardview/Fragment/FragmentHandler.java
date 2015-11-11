0387b1bbc207a32e516412bc203a77eeb07e1181
Jake King <jake.king@sky.uk>
2015-11-11 21:18:49 +0000

    Renamed Project Folder to ReadIt

---
+++ ReadIt/app/src/main/java/jake/king/sky/uk/cardview/Fragment/FragmentHandler.java
@@ -0,0 +1,37 @@
+package jake.king.sky.uk.cardview.Fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+public class FragmentHandler {
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    public FragmentHandler(FragmentManager fragmentManager){
+        this.fragmentManager = fragmentManager;
+    }
+
+    public void showLoadingFragment(View layout){
+
+        Fragment loadingFragment = new LoadingFragment();
+
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(layout.getId(), loadingFragment, "loadingFragment");
+        fragmentTransaction.commit();
+    }
+
+    public void closeLoadingFragment(){
+        Fragment loadingFragment = fragmentManager.findFragmentByTag("loadingFragment");
+
+        if(loadingFragment != null){
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(loadingFragment);
+            fragmentTransaction.commit();
+        }
+
+    }
+
+}
---
+++ ReadIt/app/src/main/java/jake/king/sky/uk/cardview/Fragment/LoadingFragment.java
@@ -0,0 +1,19 @@
+package jake.king.sky.uk.cardview.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jake.king.sky.uk.cardview.R;
+
+public class LoadingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_loading, container, false);
+        return view;
+    }
+
+}
