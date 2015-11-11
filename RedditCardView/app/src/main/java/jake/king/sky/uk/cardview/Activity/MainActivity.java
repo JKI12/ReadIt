a9efc13e8cafc08f579af94c69580fa572d6ea9f
Jake King <jake.king@sky.uk>
2015-11-11 20:52:09 +0000

    Added Icon, Implemented Loading Fragment

--- RedditCardView/app/src/main/java/jake/king/sky/uk/cardview/Activity/MainActivity.java
+++ RedditCardView/app/src/main/java/jake/king/sky/uk/cardview/Activity/MainActivity.java
@@ -1,15 +1,24 @@
 package jake.king.sky.uk.cardview.Activity;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jake.king.sky.uk.cardview.Fragment.FragmentHandler;
 import jake.king.sky.uk.cardview.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
+    private FragmentHandler fragmentHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentHandler = new FragmentHandler(fragmentManager);
+
     }
 }
