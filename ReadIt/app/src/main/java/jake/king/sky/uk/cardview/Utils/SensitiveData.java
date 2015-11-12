e0b6ccc5187d58b6c572bf9e0ae132b510010624
Jake King <jake.king@sky.uk>
2015-11-12 21:24:00 +0000

    Added base sensitive data file for file structure

---
+++ ReadIt/app/src/main/java/jake/king/sky/uk/cardview/Utils/SensitiveData.java
@@ -0,0 +1,11 @@
+package jake.king.sky.uk.cardview.Utils;
+
+/**
+ * Created by JK on 12/11/2015.
+ */
+public class SensitiveData {
+
+    public String CLIENT_ID = "";
+    public String REDIRECT_URI = "";
+
+}
